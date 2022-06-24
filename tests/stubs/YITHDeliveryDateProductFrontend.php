f31408f72ba8b04b6cc70e8380d9d6fc3c7a942b
Viktor Szépe <viktor@szepe.net>
2022-06-24 13:36:55 +0000

    Add stubs

---
+++ tests/stubs/YITHDeliveryDateProductFrontend.php
@@ -0,0 +1,13 @@
+<?php
+declare( strict_types=1 );
+
+/**
+ * Stub class of YITH_Delivery_Date_Product_Frontend for static code analyzing purposes.
+ */
+class YITH_Delivery_Date_Product_Frontend { // phpcs:ignore
+
+	/**
+	 * @return array<mixed>
+	 */
+	public function get_date_info( \WC_Product $product ): array {}
+}
